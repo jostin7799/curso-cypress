b0d045509fc6b1457b852c36ca57668650d22c0c
NazarethAguero <nazarethaguero3019@hotmail.com>
2025-05-12 11:14:19 -0600

    Creacion de la pagina Place Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testData(){
+        return{
+            name: "Test name",
+            country: "United States",
+            city: "Boston",
+            creditCard: "4012888888881881",
+            month: 12,
+            year: 2024
+        }
+    }
+}
